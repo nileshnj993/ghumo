b0c6c9258f3f2e5b8c8b74735b16fabb9c7bec89
Nilesh <nileshnj993@gmail.com>
2022-02-03 13:33:05 +0530

    search bar filtering properly. login and register backend done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const bodyparser = require("body-parser");
 const config = require('dotenv').config();
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -23,24 +24,34 @@ const saltRounds = 5;
 // });
 // const upload = multer({ storage: storage,  limits: { fileSize: 2000000 } }).single("image");
 
-// mongoose.connect("mongodb+srv://team12:"+process.env.DB_PASSWORD+"@zeus-agent.xbtgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect("mongodb+srv://team12:"+process.env.DB_PASSWORD+"@ghumo.fldxv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-// let db = mongoose.connection;
-// db.once("open", () => console.log("Connected to MongoDB"));
-// db.on("error", (err) => console.log(err));
+let db = mongoose.connection;
+db.once("open", () => console.log("Connected to MongoDB"));
+db.on("error", (err) => console.log(err));
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const userSchema = mongoose.Schema({
+    name: String,
+    password: String,
+    email: String,
+    dob: String,
+});
+const User = new mongoose.model("users", userSchema);
+
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"public","index.html"));
 })
 
 app.get("/login",(req,res)=>{
+    res.clearCookie("userEmail");
     res.sendFile(path.join(__dirname,"public","login.html"));
 })
 
@@ -67,6 +78,59 @@ app.get("/publicProfile",(req,res)=>{
 app.get("/myProfile",(req,res)=>{
     res.sendFile(path.join(__dirname,"public","myProfile.html"));
 })
+
+// CREATING ACCOUNT
+app.post("/register", async (req, res) => {
+    const user = await User.findOne({email:req.body.email});
+    if(user){
+        res.send('Account already exists');
+    }
+    else{
+    let plainPassword = req.body.password;
+    bcrypt.hash(plainPassword, saltRounds, (err, hashedPassword) => {
+      if (err) throw err;
+      signupDetails = {
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+        dob: req.body.dob,
+      };
+      User.create(signupDetails, (err, response) => {
+        if (err) {
+          res.status(500).send();
+        } else {
+          res.send('Account successfully created!');
+        }
+      });
+    });
+  }
+  });
+
+  // LOGIN
+  app.post("/home", (req, res) => {
+    loginDetails = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    User.find({ email: loginDetails.email }, (err, response) => {
+      if (err) throw err;
+      else {
+        if (Object.entries(response).length === 0) {
+          res.send('Account not found! Please Register.');
+        } else {
+          bcrypt.compare(loginDetails.password, response[0].password, (error, resp) => {
+            if (error) throw error;
+            if (resp === true) {
+              res.cookie("userEmail", loginDetails.email);
+              res.sendFile(path.join(__dirname, "public", "home.html"));
+            } else {
+              res.send("Wrong Password!");
+            }
+          });
+        }
+      }
+    });
+  });
 
 
 
